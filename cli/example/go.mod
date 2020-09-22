507e2e07b66d21a565a67a5b8b7cf3a1d4ba13d1
Vinh <thanh.vinh@hotmail.com>
2020-09-22 11:41:34 +0700

    Add gen cli

--- cli/example/go.mod
+++ cli/example/go.mod
@@ -3,7 +3,6 @@ module example
 go 1.15
 
 require (
-	github.com/ez-connect/go-rest v0.1.7
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/labstack/echo/v4 v4.1.17
 )
--- cli/gen.go
+++ cli/gen.go
@@ -1,5 +1,33 @@
-package cli
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/ez-connect/go-rest/core"
+	"github.com/ez-connect/go-rest/generator"
+)
 
 func main() {
+	workingDir := "example/routes"
+	fmt.Println("Working dir:", workingDir)
 
+	files, err := ioutil.ReadDir(workingDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			fmt.Println(f.Name())
+			c := generator.ModelConfig{}
+			err := core.LoadConfig(fmt.Sprintf("%s/%s/settings.yml", workingDir, f.Name()), &c)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println(c)
+		}
+	}
 }
