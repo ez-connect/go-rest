c81ced574edd6ea8bc604ee0b2757d88914c35f0
Vinh <thanh.vinh@hotmail.com>
2020-09-22 12:02:32 +0700

    Update gen

--- cli/gen.go
+++ cli/gen.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/ez-connect/go-rest/core"
 	"github.com/ez-connect/go-rest/generator"
 )
 
 func main() {
-	workingDir := "example/routes"
+	workingDir := "example"
 	fmt.Println("Working dir:", workingDir)
 
-	files, err := ioutil.ReadDir(workingDir)
+	files, err := ioutil.ReadDir(fmt.Sprintf("%s/routes", workingDir))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +23,22 @@ func main() {
 		if f.IsDir() {
 			fmt.Println(f.Name())
 			c := generator.ModelConfig{}
-			err := core.LoadConfig(fmt.Sprintf("%s/%s/settings.yml", workingDir, f.Name()), &c)
+			err := core.LoadConfig(fmt.Sprintf("%s/routes/%s/settings.yml", workingDir, f.Name()), &c)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			os.MkdirAll(fmt.Sprintf("%s/generated/%s", workingDir, f.Name()), os.ModeDir)
+
+			// Model
+			f, err := os.Create(fmt.Sprintf("%s/generated/%s/model.go", workingDir, f.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer f.Close()
+			v := generator.GenerateModel(f.Name(), c)
+			_, err = f.WriteString(v)
 			if err != nil {
 				log.Fatal(err)
 			}
