6328fd6450b166d362476f34926c4dc85bd36e51
trong.buido <trong.buido@gmail.com>
2020-09-22 15:35:07 +0700

    auto import

--- generator/repository.go
+++ generator/repository.go
@@ -9,11 +9,6 @@ func GenerateRepository(packageName, collection string) string {
 	buf := []string{}
 	buf = append(buf, fmt.Sprintf("package %s\n", packageName))
 
-	buf = append(buf, "import (")
-	buf = append(buf, "\t\"github.com/ez-connect/go-rest/rest\"")
-	buf = append(buf, "\t\"go.mongodb.org/mongo-driver/bson\"\n")
-	buf = append(buf, ")\n")
-
 	buf = append(buf, "type Repository struct {")
 	buf = append(buf, "\trest.RepositoryBase")
 	buf = append(buf, "\trest.RepositoryInterface")
