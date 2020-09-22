a23e4d611fefd08f814cabcc64c0cc6b779d4b01
Vinh <thanh.vinh@hotmail.com>
2020-09-22 10:33:59 +0700

    Generate routes

--- generator/handler_test.go
+++ generator/handler_test.go
@@ -11,35 +11,17 @@ var sampleHandler = `
 package test
 
 import (
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/ez-connect/go-rest/rest"
+	"github.com/labstack/echo/v4"
 )
+
+type Handler struct {
 `
 
 func TestGenerateHandler(t *testing.T) {
 	v := GenerateHandler(
 		"test",
-		ModelConfig{
-			Name: "ImAModel",
-			Attributes: []Attribute{
-				{
-					Name: "id",
-					Type: "*primitive.ObjectID",
-				},
-				{
-					Name: "name",
-					Type: "string",
-				},
-				{
-					Name: "createdAt",
-					Type: "*time.Time",
-				},
-			},
-		},
 	)
 
-	t.Error(v)
 	assert.Equal(t, true, strings.Contains(v, sampleHandler))
 }
