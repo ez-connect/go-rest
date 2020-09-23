80b4690f9e2c693d3f7737667fc25139193c0c4e
Vinh <thanh.vinh@hotmail.com>
2020-09-23 14:05:34 +0700

    Add default timestamp

--- generator/model.go
+++ generator/model.go
@@ -10,12 +10,15 @@ func GenerateModel(packageName string, config ModelConfig) string {
 	buf = append(buf, fmt.Sprintf("package %s\n", packageName))
 
 	buf = append(buf, "import (")
+	buf = append(buf, "\t\"time\"\n")
 	buf = append(buf, "\t\"go.mongodb.org/mongo-driver/bson/primitive\"")
 	buf = append(buf, ")\n")
 
 	buf = append(buf, fmt.Sprintf("const Collection%s = \"%s\"\n", strings.Title(config.Name), config.Name))
 
 	buf = append(buf, "type Model struct {")
+
+	// ObjectID
 	buf = append(buf,
 		"\tId *primitive.ObjectID `bson:\"_id,omitempty\" json:\"id,omitempty\"`",
 	)
@@ -25,7 +28,17 @@ func GenerateModel(packageName string, config ModelConfig) string {
 			strings.Title(v.Name), v.Type, v.Name, v.Name),
 		)
 	}
-	buf = append(buf, "}")
+
+	// Timestamp
+	buf = append(buf,
+		"\tCreatedAt *time.Time `bson:\"createdAt,omitempty\" json:\"createdAt,omitempty\"`",
+	)
+
+	buf = append(buf,
+		"\tUpdatedAt *time.Time `bson:\"updatedAt,omitempty\" json:\"updatedAt,omitempty\"`",
+	)
+
+	buf = append(buf, "}\n")
 
 	return strings.Join(buf, "\n")
 }
