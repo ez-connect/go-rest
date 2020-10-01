6a71244271d5a8726251d16cdb09944a1fa4360c
trong.buido <trong.buido@gmail.com>
2020-10-01 14:29:38 +0700

    Update unmarshal query

--- rest/filter/unmarshal.go
+++ rest/filter/unmarshal.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"reflect"
+	"strings"
 
 	"github.com/ez-connect/go-rest/core"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var (
@@ -17,44 +19,95 @@ var (
 		"$gt",
 		"$lt",
 	}
+
+	objectIdType = reflect.TypeOf(primitive.NewObjectID())
 )
 
-func validateMap(v map[string]interface{}, rv reflect.Value) bool {
+func validateMap(v map[string]interface{}, rv reflect.Value) bson.M {
+	result := bson.M{}
 	for key, value := range v {
 		field := rv.FieldByName(key)
 		if !field.IsValid() && core.IndexOf(keywords, key) < 0 {
-			return false
+			return nil
+		}
+
+		isObjectId := false
+		if field.IsValid() {
+			// check field is objectId
+			switch field.Kind() {
+			case reflect.Ptr:
+				isObjectId = field.Type().Elem() == objectIdType
+			case reflect.Struct:
+				isObjectId = field.Type() == objectIdType
+			}
+
+			if isObjectId && reflect.TypeOf(value).Kind() != reflect.String {
+				return nil
+			}
+
+			// get name of field from bson
+			st, found := rv.Type().FieldByName(key)
+			if found {
+				bsonInfo := strings.Split(st.Tag.Get("bson"), ",")
+				if len(bsonInfo) > 0 {
+					key = bsonInfo[0]
+				}
+			}
 		}
 
 		switch value := value.(type) {
 		case map[string]interface{}:
-			if !validateMap(value, rv) {
-				return false
+			if result[key] = validateMap(value, rv); result[key] == nil {
+				return nil
 			}
 		case []interface{}:
-			if !validateArray(value, rv) {
-				return false
+			if result[key] = validateArray(value, rv); result[key] == nil {
+				return nil
 			}
+		case string:
+			if isObjectId {
+				objectId, err := primitive.ObjectIDFromHex(value)
+				if err != nil {
+					return nil
+				}
+				switch field.Kind() {
+				case reflect.Ptr:
+					result[key] = &objectId
+				case reflect.Struct:
+					result[key] = objectId
+				}
+			} else {
+				result[key] = value
+			}
+		default:
+			result[key] = value
 		}
 
 	}
-	return true
+	return result
 }
 
-func validateArray(v []interface{}, rv reflect.Value) bool {
+func validateArray(v []interface{}, rv reflect.Value) []interface{} {
+	result := make([]interface{}, 0)
 	for value := range v {
-		switch v[value].(type) {
+		switch x := v[value].(type) {
 		case map[string]interface{}:
-			if !validateMap(v[value].(map[string]interface{}), rv) {
-				return false
+			t := validateMap(x, rv)
+			if t == nil {
+				return nil
 			}
+			result = append(result, t)
 		case []interface{}:
-			if !validateArray(v[value].([]interface{}), rv) {
-				return false
+			t := validateArray(x, rv)
+			if t == nil {
+				return nil
 			}
+			result = append(result, t)
+		default:
+			result = append(result, x)
 		}
 	}
-	return true
+	return result
 }
 
 func Unmarshal(query string, v interface{}) map[string]interface{} {
@@ -75,9 +128,5 @@ func Unmarshal(query string, v interface{}) map[string]interface{} {
 
 	// validate query
 	s := rv.Elem()
-	if validateMap(res, s) {
-		return res
-	} else {
-		return nil
-	}
+	return validateMap(res, s)
 }
