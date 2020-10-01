a2016303fc803b7bf4803c0a625d2e0422b61379
Vinh <thanh.vinh@hotmail.com>
2020-10-01 15:33:58 +0700

    Drop sort order of single-field index https://docs.mongodb.com/manual/indexes/

--- cli/generator/config.go
+++ cli/generator/config.go
@@ -15,10 +15,20 @@ type ModelConfig struct {
 	Attributes []Attribute `yaml:"attributes"`
 }
 
+// Will support compound order
+// For a single-field index and sort operations,
+// the sort order (i.e. ascending or descending)
+// of the index key does not matter because MongoDB
+// can traverse the index in either direction.
+// https://docs.mongodb.com/manual/indexes/
+type CompoundIndex struct {
+	Field string
+	Order int
+}
+
 type Index struct {
 	Fields []string `yaml:"fields"`
-	Order  int      `yaml:"order,omitempty"` // 1: asc, -1: desc
-	Text   bool     `yaml:"text,omitempty"`  // text index
+	Text   bool     `yaml:"text,omitempty"` // text index
 	Unique bool     `yaml:"unique,omitempty"`
 }
 
