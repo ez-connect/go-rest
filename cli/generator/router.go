a6918e92e0c942ee4ade8eb806ace1aaf3632ecf
conruacodon <trong.buido@gmail.com>
2020-10-05 16:23:01 +0700

    Update path param a bit (Id -> id) Add middleware func to check permission

--- cli/generator/router.go
+++ cli/generator/router.go
@@ -13,6 +13,12 @@ func GenerateRoutes(packageName string, config Config) string {
 	buf = append(buf, "\t\"github.com/ez-connect/go-rest/db\"")
 	buf = append(buf, "\t\"github.com/labstack/echo/v4\"\n")
 	buf = append(buf, fmt.Sprintf("\t\"app/services/%s\"", packageName))
+
+	// add import from settings
+	for _, i := range config.RouteFile.Imports {
+		buf = append(buf, fmt.Sprintf("\t\"%s\"\n", i))
+	}
+
 	buf = append(buf, ")\n")
 
 	buf = append(buf, "type Router struct {")
@@ -25,8 +31,11 @@ func GenerateRoutes(packageName string, config Config) string {
 	buf = append(buf, "\th.Repo.Init(db)")
 	buf = append(buf, "\th.Repo.EnsureIndexs()\n")
 
-	for i, v := range config.Routes {
+	for i, v := range config.RouteFile.Routes {
 		buf = append(buf, fmt.Sprintf("\tg%v := e.Group(\"%s\")", i, v.Path))
+		if v.MiddlewareFunc != "" {
+			buf = append(buf, fmt.Sprintf("\tg%v.Use(rest.JWTWithAuthHandler(%s))", i, v.MiddlewareFunc))
+		}
 		for _, r := range v.Children {
 			buf = append(buf,
 				fmt.Sprintf("\tg%v.%s(\"%s\", h.%s)", i, r.Method, r.Path, r.Handler),
--- cli/generator/settings.go
+++ cli/generator/settings.go
@@ -28,32 +28,35 @@ func GenerateSettings(packageName string) string {
 				Fields: []string{"price"},
 			},
 		},
-		Routes: []RouteGroup{
-			{
-				Path: fmt.Sprintf("/%ss", packageName),
-				Children: []RouteConfig{
-					{
-						Method:  http.MethodGet,
-						Handler: fmt.Sprintf("Find%s", strings.Title(packageName)),
-					},
-					{
-						Method:  http.MethodPost,
-						Handler: fmt.Sprintf("Insert%s", strings.Title(packageName)),
-					},
-					{
-						Method:  http.MethodGet,
-						Path:    "/:Id",
-						Handler: fmt.Sprintf("FindOne%s", strings.Title(packageName)),
-					},
-					{
-						Method:  http.MethodPut,
-						Path:    "/:Id",
-						Handler: fmt.Sprintf("Update%s", strings.Title(packageName)),
-					},
-					{
-						Method:  http.MethodDelete,
-						Path:    "/:Id",
-						Handler: fmt.Sprintf("Delete%s", strings.Title(packageName)),
+		RouteFile: RouteFileConfig{
+			[]string{},
+			[]RouteGroup{
+				{
+					Path: fmt.Sprintf("/%ss", packageName),
+					Children: []RouteConfig{
+						{
+							Method:  http.MethodGet,
+							Handler: fmt.Sprintf("Find%s", strings.Title(packageName)),
+						},
+						{
+							Method:  http.MethodPost,
+							Handler: fmt.Sprintf("Insert%s", strings.Title(packageName)),
+						},
+						{
+							Method:  http.MethodGet,
+							Path:    "/:Id",
+							Handler: fmt.Sprintf("FindOne%s", strings.Title(packageName)),
+						},
+						{
+							Method:  http.MethodPut,
+							Path:    "/:Id",
+							Handler: fmt.Sprintf("Update%s", strings.Title(packageName)),
+						},
+						{
+							Method:  http.MethodDelete,
+							Path:    "/:Id",
+							Handler: fmt.Sprintf("Delete%s", strings.Title(packageName)),
+						},
 					},
 				},
 			},
